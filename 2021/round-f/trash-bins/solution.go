912d46108b594fc975274d47a8998dde1adc3d1f
mpi3d <matthieu.charette@gmail.com>
2021-09-19 10:10:57 +0200

    Simplified program for round f

--- 2021/round-f/trash-bins/solution.go
+++ 2021/round-f/trash-bins/solution.go
@@ -5,7 +5,10 @@ import (
 	"math"
 )
 
-func l(n int, i int, s []bool) float64 {
+var t, n int
+var s []bool
+
+func l(i int) float64 {
 	if i == 0 {
 		return math.Inf(-1)
 	}
@@ -14,10 +17,10 @@ func l(n int, i int, s []bool) float64 {
 		return float64(i - 1)
 	}
 
-	return l(n, i-1, s)
+	return l(i - 1)
 }
 
-func r(n int, i int, s []bool) float64 {
+func r(i int) float64 {
 	if i == n-1 {
 		return math.Inf(1)
 	}
@@ -26,37 +29,34 @@ func r(n int, i int, s []bool) float64 {
 		return float64(i + 1)
 	}
 
-	return r(n, i+1, s)
+	return r(i + 1)
 }
 
-func f(n int, i int, s []bool) int {
+func f(i int) int {
 	if s[i] {
 		return 0
 	}
 
-	return int(math.Min(float64(i)-l(n, i, s), r(n, i, s)-float64(i)))
+	return int(math.Min(float64(i)-l(i), r(i)-float64(i)))
 }
 
 func solve() int {
-	n := 0
 	fmt.Scan(&n)
 
-	s := make([]bool, n)
+	s = make([]bool, n)
 	for i := 0; i < n; i++ {
 		fmt.Scan(&s[i])
 	}
 
 	a := 0
 	for i := 0; i < n; i++ {
-		a += f(n, i, s)
+		a += f(i)
 	}
 
 	return a
 }
 
 func main() {
-	var t int
-
 	fmt.Scan(&t)
 
 	for i := 0; i < t; i++ {
