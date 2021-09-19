187df5b4f7ca315ab34724012378d8958231d650
mpi3d <matthieu.charette@gmail.com>
2021-09-19 09:52:07 +0200

    Updated round f with solution

--- 2021/round-f/trash-bins/solution.go
+++ 2021/round-f/trash-bins/solution.go
@@ -2,58 +2,64 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func solve() {
+func l(n int, i int, s []bool) float64 {
+	if i == 0 {
+		return math.Inf(-1)
+	}
+
+	if s[i-1] {
+		return float64(i - 1)
+	}
+
+	return l(n, i-1, s)
+}
+
+func r(n int, i int, s []bool) float64 {
+	if i == n-1 {
+		return math.Inf(1)
+	}
+
+	if s[i+1] {
+		return float64(i + 1)
+	}
+
+	return r(n, i+1, s)
+}
+
+func f(n int, i int, s []bool) int {
+	if s[i] {
+		return 0
+	}
+
+	return int(math.Min(float64(i)-l(n, i, s), r(n, i, s)-float64(i)))
+}
+
+func solve() int {
 	n := 0
 	fmt.Scan(&n)
 
-	t := make([]bool, n)
+	s := make([]bool, n)
 	for i := 0; i < n; i++ {
-		fmt.Scan(&t[i])
+		fmt.Scan(&s[i])
 	}
 
-	c := 0
+	a := 0
 	for i := 0; i < n; i++ {
-		if !t[i] {
-			r := 0
-			for j := i + 1; j < n; j++ {
-				if t[j] {
-					r = j - i
-					break
-				}
-			}
-
-			l := 0
-			for j := i - 1; j >= 0; j-- {
-				if t[j] {
-					l = i - j
-					break
-				}
-			}
-
-			if r == 0 {
-				c += l
-			} else if l == 0 {
-				c += r
-			} else if r > l {
-				c += l
-			} else {
-				c += r
-			}
-		}
+		a += f(n, i, s)
 	}
 
-	fmt.Print(c)
+	return a
 }
 
 func main() {
 	var t int
+
 	fmt.Scan(&t)
-	t++
-	for i := 1; i < t; i++ {
-		fmt.Print("Case #", i, ": ")
-		solve()
-		fmt.Println()
+
+	for i := 0; i < t; i++ {
+		fmt.Printf("Case #%d: %d\n", i+1, solve())
 	}
 }
